2c782ed52081a8649c0fdaf58cffa039f1d305dd
amartins25 <alexandremartins13rj@gmail.com>
2019-05-15 20:31:59 -0300

    exerc de js pelo digo

---
+++ exercicio-maio.js
@@ -0,0 +1,15 @@
+/*function somar(numero1,numero2) {
+    let resultado = (numero1 + numero2)
+    return resultado;
+    }
+
+    console.log(somar(20,12))
+
+*/
+
+/*let deuses = ["Aegir","Aud","Balder"];
+    function name(params) {
+        
+    }*/
+
+
